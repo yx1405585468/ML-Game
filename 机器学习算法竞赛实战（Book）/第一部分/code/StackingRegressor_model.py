e8785fbb1b100021954a44d9aa0d29e4ecc7eb1a
yu.xin <1405585468@qq.com>
2024-10-30 15:18:24 +0800

    1

---
+++ 机器学习算法竞赛实战（Book）/第一部分/code/StackingRegressor_model.py
@@ -0,0 +1,9 @@
+import numpy as np
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import Ridge, Lasso
+from sklearn.model_selection import KFold
+from math import sqrt
+
+
