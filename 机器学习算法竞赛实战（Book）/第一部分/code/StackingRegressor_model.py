4a3d2adca05773acb183e009efb414d4a52d5106
yu.xin <1405585468@qq.com>
2024-10-31 12:32:50 +0800

    11

--- 机器学习算法竞赛实战（Book）/第一部分/code/StackingRegressor_model.py
+++ 机器学习算法竞赛实战（Book）/第一部分/code/StackingRegressor_model.py
@@ -1,9 +1,73 @@
-import numpy as np
-from sklearn.ensemble import ExtraTreesRegressor
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
-from sklearn.linear_model import Ridge, Lasso
+import pandas as pd
 from sklearn.model_selection import KFold
-from math import sqrt
+
+# 设置 Pandas 显示选项以显示所有列
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1000)
+
+# 打上intel优化补丁
+from sklearnex import patch_sklearn
+
+patch_sklearn()
 
 
+class SklearnModelStacking:
+    def __init__(self, model_params, seed=2024):
+        self.model_list = []
+        self.split_model_list = []
+        self.train_data_f2 = None
+        self.test_data_f2 = None
+        for item in model_params:
+            model = item["model"](random_state=seed, **item["params"])
+            self.model_list.append(model)
+
+    def fit(self, X, y):
+        # 对外部有可能是分割来的数据集进行重置索引
+        X = X.reset_index(drop=True)
+        y = y.reset_index(drop=True)
+
+        # 定义好多个模型的预测结果
+        result = []
+
+        kf = KFold(n_splits=5, shuffle=True, random_state=2024)
+        for i, model in enumerate(self.model_list):
+            temp_model_list = []
+            train_data = pd.DataFrame([None] * y.shape[0], columns=[i])
+            for train_idx, valid_idx in kf.split(X, y):
+                train_x, train_y = X.loc[train_idx], y.loc[train_idx]
+                valid_x, valid_y = X.loc[valid_idx], y.loc[valid_idx]
+                model.fit(train_x, train_y)
+                train_data.loc[valid_idx, i] = model.predict(valid_x)
+                temp_model_list.append(model)
+            result.append(train_data)
+            self.split_model_list.append(temp_model_list)
+        self.train_data_f2 = pd.concat(result, axis=1)
+        self.train_data_f2['label'] = y
+
+    def predict(self, X_test, stack_model):
+        result = []
+        for i, models in enumerate(self.split_model_list):
+            test_data = pd.Series(0, index=range(X_test.shape[0]))
+            for i, model in enumerate(models):
+                test_data = test_data + model.predict(X_test)
+            test_data = test_data / (i + 1)
+            result.append(test_data)
+        self.test_data_f2 = pd.concat(result, axis=1)
+        return self.true_predict(stack_model)
+
+    def true_predict(self, stack_model):
+        X = self.train_data_f2.iloc[:, :-1]
+        y = self.train_data_f2.iloc[:, -1]
+        X_test = self.test_data_f2
+        model = stack_model()
+
+        kf = KFold(n_splits=5, shuffle=True, random_state=2024)
+        pre_data = pd.Series(0, index=range(X_test.shape[0]))
+        i = 0
+        for j, (train_idx, valid_idx) in enumerate(kf.split(X, y)):
+            train_x, train_y = X.loc[train_idx], y.loc[train_idx]
+            model.fit(train_x, train_y)
+            pre_data = pre_data + model.predict(X_test)
+            i = j
+        pre_data = pre_data / (i + 1)
+        return pre_data
---
+++ 机器学习算法竞赛实战（Book）/第一部分/code/main.py
@@ -0,0 +1,44 @@
+from sklearn.datasets import fetch_california_housing
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from StackingRegressor_model import SklearnModelStacking
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+if __name__ == '__main__':
+    # 导入加利福尼亚房价数据集
+    california_housing = fetch_california_housing()
+
+    # 创建 DataFrame
+    data = pd.DataFrame(data=california_housing.data, columns=california_housing.feature_names)
+
+    # 添加目标列
+    data['target'] = california_housing.target
+    model_params_ = [
+        {
+            "model": RandomForestRegressor,
+            "params": {'n_estimators': 100, 'max_features': 0.2, 'max_depth': 12, 'min_samples_leaf': 2}
+        },
+        {
+            "model": ExtraTreesRegressor,
+            "params": {'n_estimators': 100, 'max_features': 0.2, 'max_depth': 12, 'min_samples_leaf': 2}
+        }
+    ]
+
+    model = SklearnModelStacking(model_params_)
+    X = data.iloc[:, :-1]
+    y = data.iloc[:, -1]
+
+    # 构造训练集与测试集
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+    model.fit(X_train, y_train)
+    pre = model.predict(X_test, RandomForestRegressor)
+    print(mean_squared_error(y_test, pre))
+
+    param = {'n_estimators': 100, 'max_features': 0.2, 'max_depth': 12, 'min_samples_leaf': 2}
+    model = ExtraTreesRegressor(**param)
+    model.fit(X_train, y_train)
+    pre = model.predict(X_test)
+    print(mean_squared_error(y_test, pre))
